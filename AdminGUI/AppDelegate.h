43530f358f106b502cf725892d66dd91c43597cf
Brian Choi <bchoi@Brians-MacBook-Air.local>
2014-05-02 13:29:13 -0700

    UserGUI

---
+++ AdminGUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AdminGUI
+//
+//  Created by Brian Choi on 5/1/14.
+//  Copyright (c) 2014 Brian Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AdminGUI/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  AdminGUI
+//
+//  Created by Brian Choi on 5/1/14.
+//  Copyright (c) 2014 Brian Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
